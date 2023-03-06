5453c89c9a462706bc5fda5a03ed037ac229b86f
hakim220619 <danilukman2206@gmail.com>
2023-03-07 00:08:23 +0700

    login

---
+++ lib/service/http_service.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:loginwithapi/views/dashboard.dart';
+import 'package:loginwithapi/views/welcome.dart';
+
+class HttpService {
+  static final _client = http.Client();
+
+  static var _loginUrl = Uri.parse('https://travel.dlhcode.com/api/login');
+
+  static var _registerUrl =
+      Uri.parse('https://travel.dlhcode.com/api/register');
+
+  static login(email, password, context) async {
+    http.Response response = await _client.post(_loginUrl, body: {
+      "email": email,
+      "password": password,
+    });
+
+    if (response.statusCode == 200) {
+      // print(jsonDecode(response.body));
+      // var json = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        // await EasyLoading.showSuccess(json[0]);
+        await Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Dashboard()));
+      } else {
+        // EasyLoading.showError(json[0]);
+      }
+    } else {
+      await EasyLoading.showError(
+          "Error Code : ${response.statusCode.toString()}");
+    }
+  }
+
+  static register(email, password, nama, noHp, role, context) async {
+    http.Response response = await _client.post(_registerUrl, body: {
+      "email": email,
+      "password": password,
+      "nama": nama,
+      "no_hp": noHp,
+      "role_id": role,
+    });
+    print(response);
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.body.toString());
+      print(json);
+      if (json == 'username already exist') {
+        print(json);
+        await EasyLoading.showError(json);
+      } else {
+        print(json);
+        // await EasyLoading.showSuccess(json.success);
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Dashboard()));
+      }
+    } else {
+      await EasyLoading.showError(
+          "Error Code : ${response.statusCode.toString()}");
+    }
+  }
+}
---
+++ lib/views/login.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:loginwithapi/service/http_service.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:loginwithapi/views/register.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late String email;
+  late String password;
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Sign in',
+                style: TextStyle(fontSize: 20),
+              )),
+          Container(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: TextFormField(
+              obscureText: false,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              maxLines: 1,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  prefixIcon: const Icon(Icons.email),
+                  labelText: 'Masukan Email',
+                  hintText: 'Masukan Email'),
+              onChanged: (value) {
+                setState(() {
+                  email = value;
+                });
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              maxLines: 1,
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(90.0),
+                  ),
+                  prefixIcon: const Icon(Icons.lock),
+                  labelText: 'Masukan Password',
+                  hintText: 'Masukan Password'),
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                print(password);
+                print(email);
+                HttpService.login(email, password, context);
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+            ),
+            child: const Text(
+              'Sign in',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Not registered yet?'),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterPage(),
+                    ),
+                  );
+                },
+                child: const Text('Create an account'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
